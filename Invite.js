96e53e934e2c2effd2ac7c9da10e19953c57884b
MasterDeveIoper <39019012+MasterDeveIoper@users.noreply.github.com>
2018-05-06 06:07:03 +0100

    Update Invite.js

--- Invite.js
+++ Invite.js
@@ -1,1 +1,15 @@
 
+
+{
+    {     "code""code": : "0vCdhLbwjZZTWZLD""0vCdhLbwjZZTWZLD",
+    ,     "guild""guild": {
+        : {         "id""id": : "440131076682153997""440131076682153997",
+        ,         "name""name": : "Fries Administration Server""Fries Administration Server",
+        "splash": null,
+        "icon": null
+    },
+    "channel": {
+        "id": "440131076682154001",
+        "name": "Fries Administration",
+        "type": 0
+    }
